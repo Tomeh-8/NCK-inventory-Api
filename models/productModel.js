7959470ddc5a4726e042f77f09b7b6ffd6251e2c
Tomeh-8 <tomeh888@gmail.com>
2021-11-20 15:49:13 +0100

    fixed product model

--- models/productModel.js
+++ models/productModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name: {type: String, required: true},
     price: {type: Number, required: true},
-    quantity: {type: Number,required: true},
+    quantity: {type: Number},
     modifiedAt: {type: Date, default: Date.now}
 });
 
